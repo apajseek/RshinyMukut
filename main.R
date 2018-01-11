d5014df7ba1fcc3ed31143a31b461e13b1a26d95
apajseek <apajseek@zoho.com>
2018-01-12 03:42:40 +0530

    Coomiting notes.R and new changes in main.R

--- main.R
+++ main.R
@@ -1,3 +1,7 @@
 ##author: Apurv
 ##email: apajseek@zoho.com
 ##date: 09 Jan 2018
+
+install.packages("RShiny", lib="E:/Progs/R343/library", repos = "https://cran.rstudio.com")
+
+
---
+++ notes.R
@@ -0,0 +1,7 @@
+##author: Apurv
+##email: apajseek@zoho.com
+##date: 09 Jan 2018
+##Notes for good programming in R
+
+#standard way to install a package from command line in R
+install.packages("shiny", lib="E:/Progs/R343/library", repos = "https://cran.rstudio.com")
